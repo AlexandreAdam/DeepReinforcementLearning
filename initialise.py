dd480284624b87c8f4fdad062d1820f06061f5cc
epfle <e.pfleiderer@hotmail.com>
2019-05-16 22:03:18 -0400

    Uploading trained Model 1 (66 versions)

--- initialise.py
+++ initialise.py
@@ -3,5 +3,5 @@ Used to specify model information during loading process.
 """
 
 INITIAL_RUN_NUMBER = 1
-INITIAL_MODEL_VERSION = None
-INITIAL_MEMORY_VERSION = None
+INITIAL_MODEL_VERSION = 25
+INITIAL_MEMORY_VERSION = 65
