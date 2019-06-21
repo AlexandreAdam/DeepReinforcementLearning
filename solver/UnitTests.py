3f195a885e4b8a5ae15e9bc69faf7390d2816273
epfle <e.pfleiderer@hotmail.com>
2019-06-21 01:18:51 -0400

    Added new test (first_move).

--- solver/UnitTests.py
+++ solver/UnitTests.py
@@ -8,6 +8,7 @@ These unit tests assume that both players play perfectly. The optimal move is kn
 
 
 # The first player has successfully set up a double menace and is guaranteed to win in two moves.
+# Takes a few seconds to compute.
 def double_menace():
 
     print('|-------------------------------------------------------------|')
@@ -43,6 +44,7 @@ def double_menace():
 
 # The first player is trying to set up a double menace.
 # The second player must counter it at this turn or he is guaranteed to lose.
+# Takes a few seconds to compute.
 def counter_double_menace():
 
     print('|-------------------------------------------------------------|')
@@ -71,7 +73,32 @@ def counter_double_menace():
     print('\n TEST SUCCESSFUL:', solver.nodes_explored, ' nodes explored in ', round(end_time-start_time, 2), 'seconds.')
     print('|-------------------------------------------------------------|\n')
 
-if __name__ == '__main__':
-    double_menace()
-    counter_double_menace()
 
+# The first player has to choose his first move.
+# Takes about 3 minutes to compute.
+def first_move():
+
+    print('|-------------------------------------------------------------|')
+    print('TEST OBJECTIVE: choose the optimal first move\n')
+
+    start_time = time()
+
+    shape = (4, 5)
+    game = DynamicGame(grid_shape=shape)
+
+    print('\n', game)
+    solver = Solver(game.gameState)
+    game.step(solver.get_action())
+
+    end_time = time()
+
+    assert (game.gameState.board[17] == 1)
+
+    print('\n TEST SUCCESSFUL:', solver.nodes_explored, ' nodes explored in ', round(end_time-start_time, 2), 'seconds.')
+    print('|-------------------------------------------------------------|\n')
+
+
+if __name__ == '__main__':
+    # double_menace()
+    # counter_double_menace()
+    first_move()
